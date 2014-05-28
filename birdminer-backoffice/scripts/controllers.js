252d5d5ca31f0e204e65c77c22a8a09ad6a9f0f2
christof <christof@photogenity.com>
2014-05-28 16:17:59 +0200

    Simple AngularJS controller

---
+++ birdminer-backoffice/scripts/controllers.js
@@ -0,0 +1,31 @@
+function candidateController($scope) {
+
+    $scope.candidates = [
+        {message: 'walk the dog', done: false},
+        {message: 'feed the cat', done: false}
+    ];
+
+    $scope.returnTotalCandidates = function () {
+        console.log ('returnTotalCandidates executes');
+        return $scope.candidates.length;
+    };
+
+
+    /*$scope.addNewTodo = function () {
+        console.log ('addNewTodo executes');
+        var nothing = $scope.todos.length;
+        var nothing2  = nothing;
+        if ($scope.newTodoText.length){
+            $scope.todos.push ( {todoItem: $scope.newTodoText , done: false});
+            $scope.newTodoText = '';
+        }
+
+
+    }
+
+    $scope.clearFinishedTodos = function () {
+        console.log ('clearFinishedTodos executes');
+        $scope.todos = _.filter($scope.todos, function (todo) {return !todo.done})
+    }
+     */
+}

e7b5a402bdfe0ab20d63903bff701dbc2fcebe18
christof <christof@photogenity.com>
2014-05-28 17:32:16 +0200

    Working Highlight Table (na ja)

--- birdminer-backoffice/scripts/controllers.js
+++ birdminer-backoffice/scripts/controllers.js
@@ -1,9 +1,6 @@
 function candidateController($scope, $http) {
 
-    $scope.candidates = [
-        {message: 'walk the dog', done: false},
-        {message: 'feed the cat', done: false}
-    ];
+    $scope.candidates = [];
 
     $scope.returnTotalCandidates = function () {
         console.log ('returnTotalCandidates executes');
@@ -19,7 +16,26 @@ function candidateController($scope, $http) {
                 var candidates = data.hits.hits;
                 console.log(data);
                 angular.forEach(candidates, function(value, key) {
-                    $scope.candidates.push({ file: value._source.file, percolators: value._source.percolators });
+                    var documentId = value._id;
+
+                    angular.forEach(value._source.percolators, function(value, key) {
+                        $http({method: 'GET', url: 'http://localhost:9200/birding/.percolator/' + value}).
+                            success(function(data, status, headers, config) {
+                                var payload = { query: data._source.query, filter: { ids: { values: [ documentId ] } }, highlight: { fields: { text: {}}} };
+                                $http.post('http://localhost:9200/result/bird_candidate/_search',
+                                    payload).
+                                    success(function(data, status, headers, config) {
+                                        console.log("highlight" + data.hits.hits[0].highlight.text[0]);
+                                        for (var i = 0; i < $scope.candidates.length; i++) {
+                                            if ($scope.candidates[i].id === documentId) {
+                                                $scope.candidates[i].highlight = $scope.candidates[i].highlight + '\n' + data.hits.hits[0].highlight.text[0];
+                                            }
+                                        }
+                                    });
+                            });
+                    });
+
+                    $scope.candidates.push({ id: documentId, file: value._source.file, percolators: value._source.percolators });
                 });
             }).
             error(function(data, status, headers, config) {
@@ -31,17 +47,9 @@ function candidateController($scope, $http) {
         return true;
     };
 
-    /*$scope.addNewTodo = function () {
-        console.log ('addNewTodo executes');
-        var nothing = $scope.todos.length;
-        var nothing2  = nothing;
-        if ($scope.newTodoText.length){
-            $scope.todos.push ( {todoItem: $scope.newTodoText , done: false});
-            $scope.newTodoText = '';
-        }
-
-
-    }                             */
+    $scope.showCandidate = function () {
+        console.log ('showCandidate executes');
+    }
 
     $scope.clearCandidates = function () {
         console.log ('clear executes');

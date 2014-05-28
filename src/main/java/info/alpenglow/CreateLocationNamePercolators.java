10908ef12e63161aad74252719fa3f16618bc9c7
christof <christof@photogenity.com>
2014-05-28 13:28:10 +0200

    Same thing for the location names

---
+++ src/main/java/info/alpenglow/CreateLocationNamePercolators.java
@@ -0,0 +1,43 @@
+package info.alpenglow;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CreateLocationNamePercolators {
+
+    private static Client client;
+
+    public static Client getClient() {
+        return client;
+    }
+
+    private static void createPercolator(String locationName) {
+        //TODO: Code goes here
+        System.out.println("Creating percolator for " + locationName);
+    }
+
+    private static void createLocationNamePercolators(String inputFile) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            createPercolator(line);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Connect to ElasticSearch
+        client = new TransportClient()
+                .addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
+
+        // Load the file with bird names and create the percolators
+        createLocationNamePercolators("input/Hotspots Nova Scotia.txt");
+
+        // Close ElasticSearch
+        getClient().close();
+    }
+}

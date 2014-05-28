1bf8dd60ea6fa7128728a1ae9aa5473dc8bf623f
christof <christof@photogenity.com>
2014-05-28 16:40:03 +0200

    Ajax call to retrieve candidates

--- birdminer-backoffice/scripts/controllers.js
+++ birdminer-backoffice/scripts/controllers.js
@@ -1,4 +1,4 @@
-function candidateController($scope) {
+function candidateController($scope, $http) {
 
     $scope.candidates = [
         {message: 'walk the dog', done: false},
@@ -12,7 +12,23 @@ function candidateController($scope) {
 
     $scope.refreshCandidateList= function () {
         console.log ('refreshCandidateList executes');
-        return $scope.candidates.length;
+        $scope.clearCandidates();
+        $http({method: 'POST', url: 'http://localhost:9200/result/_search'}).
+            success(function(data, status, headers, config) {
+                console.log("Status is", status);
+                var candidates = data.hits.hits;
+                console.log(data);
+                angular.forEach(candidates, function(value, key) {
+                    $scope.candidates.push({ file: value._source.file, perculators: value._source.perculators });
+                });
+            }).
+            error(function(data, status, headers, config) {
+                console.log("Error! is ", status);
+                // called asynchronously if an error occurs
+                // or server returns response with status
+                // code outside of the <200, 400) range
+            });
+        return true;
     };
 
     /*$scope.addNewTodo = function () {
@@ -25,11 +41,10 @@ function candidateController($scope) {
         }
 
 
-    }
+    }                             */
 
-    $scope.clearFinishedTodos = function () {
-        console.log ('clearFinishedTodos executes');
-        $scope.todos = _.filter($scope.todos, function (todo) {return !todo.done})
+    $scope.clearCandidates = function () {
+        console.log ('clear executes');
+        $scope.candidates = [];
     }
-     */
 }

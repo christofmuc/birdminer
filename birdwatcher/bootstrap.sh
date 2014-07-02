9bc6a9bc6faef7a355f4ef71923518b6eea279df
christof <christof@photogenity.com>
2014-07-03 00:34:08 +0200

    Adding Vagrant setup for a machine running fluentd
    And configuring fluentd to listen to all twitter messages containing
    #bird

---
+++ birdwatcher/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Base installation
+apt-get --yes update
+apt-get --yes install build-essential
+apt-get --yes install ruby
+
+# Install Fluentd in the td-agent version
+apt-get --yes install curl
+curl -L http://toolbelt.treasuredata.com/sh/install-ubuntu-lucid.sh | sh
+
+# Install the Twitter Plugin for fluentd
+apt-get --yes install libssl-dev
+sudo /usr/lib/fluent/ruby/bin/fluent-gem install fluent-plugin-twitter
+
+# Configure fluentd
+if [ ! -f ~/td-agent.conf.original ]; then cp /etc/td-agent/td-agent.conf ~/td-agent.conf.original; fi;
+cat ~/td-agent.conf.original /vagrant/twitter-source-fluentd.txt > td-agent.conf
+sudo cp td-agent.conf /etc/td-agent/td-agent.conf
+
+# Fire up FluentD
+sudo /etc/init.d/td-agent start

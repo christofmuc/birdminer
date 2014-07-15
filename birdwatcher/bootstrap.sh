27862cc6542f1ea24a7652c3e8713a1f97244831
christof <christof@photogenity.com>
2014-07-15 22:33:23 +0200

    Changed: Install puppet modules via shell script, don't put them into the source repository.

--- birdwatcher/bootstrap.sh
+++ birdwatcher/bootstrap.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
 # Base installation
-apt-get --yes update
-apt-get --yes install build-essential
-apt-get --yes install ruby
+#apt-get --yes update
+#apt-get --yes install build-essential
+#apt-get --yes install ruby
 
 # Install Puppet modules used later
 mkdir -p /etc/puppet/modules
 puppet module install puppetlabs/apt
+puppet module install puppetlabs-concat
+puppet module install puppetlabs-apache
+puppet module install srf-fluentd
+puppet module install puppetlabs-stdlib
+puppet module install puppetlabs-vcsrepo
 puppet module install elasticsearch-elasticsearch
-
-# This is one of many Kibana puppet modules
 puppet module install thejandroman-kibana3
 
 # Fix Puppet Ruby installation on Ubuntu Lucid
 # https://ask.puppetlabs.com/question/2147/could-not-find-a-suitable-provider-for-augeas/
 # http://m0dlx.com/blog/Puppet__could_not_find_a_default_provider_for_augeas.html
 # https://launchpad.net/~raphink/+archive/augeas
-sudo add-apt-repository ppa:raphink/augeas
-sudo apt-get install libaugeas0
-sudo /opt/vagrant_ruby/bin/gem install ruby-augeas 
+#sudo add-apt-repository ppa:raphink/augeas
+#sudo apt-get install libaugeas0
+#sudo /opt/vagrant_ruby/bin/gem install ruby-augeas
 
 #sudo apt-get --yes install libaugeas-dev
 #sudo apt-get install libxml2-dev
@@ -27,20 +30,20 @@ sudo /opt/vagrant_ruby/bin/gem install ruby-augeas
 
 
 # Install Fluentd in the td-agent version
-apt-get --yes install curl
-curl -L http://toolbelt.treasuredata.com/sh/install-ubuntu-lucid.sh | sh
+#apt-get --yes install curl
+#curl -L http://toolbelt.treasuredata.com/sh/install-ubuntu-lucid.sh | sh
 
 # Install the Twitter Plugin for fluentd
-apt-get --yes install libssl-dev
-sudo /usr/lib/fluent/ruby/bin/fluent-gem install fluent-plugin-twitter
+#apt-get --yes install libssl-dev
+#sudo /usr/lib/fluent/ruby/bin/fluent-gem install fluent-plugin-twitter
 
 # Install Elasticsearch Plugin for fluentd
-sudo apt-get --yes install libcurl4-gnutls-dev
-sudo /usr/lib/fluent/ruby/bin/fluent-gem install fluent-plugin-elasticsearch
+#sudo apt-get --yes install libcurl4-gnutls-dev
+#sudo /usr/lib/fluent/ruby/bin/fluent-gem install fluent-plugin-elasticsearch
 
 # Configure fluentd
-if [ ! -f ~/td-agent.conf.original ]; then cp /etc/td-agent/td-agent.conf ~/td-agent.conf.original; fi;
-sudo cp /vagrant/td-agent.conf /etc/td-agent/td-agent.conf
+#if [ ! -f ~/td-agent.conf.original ]; then cp /etc/td-agent/td-agent.conf ~/td-agent.conf.original; fi;
+#sudo cp /vagrant/td-agent.conf /etc/td-agent/td-agent.conf
 
 # Fire up FluentD
-sudo /etc/init.d/td-agent start
+#sudo /etc/init.d/td-agent start

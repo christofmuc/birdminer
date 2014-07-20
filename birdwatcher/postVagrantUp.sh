792f6d30b74bcdb100c1abf41c6a9956651c6252
Tilman Schlenker <tilman.s@me.com>
2014-07-20 17:34:04 +0200

    Cleanup the merge

--- birdwatcher/postVagrantUp.sh
+++ birdwatcher/postVagrantUp.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Deploy the Kibana dashboards to the file system
-sudo cp /vagrant/kibana/* /opt/kibana3/src/app/dashboards/
-
 # Make sure we have mappings in place for the percolator result fields - else, ElasticSearch breaks at spaces
 curl -XPUT 'http://localhost:9200/facebook/post/1' -d '
 {
